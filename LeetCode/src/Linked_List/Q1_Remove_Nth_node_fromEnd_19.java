15c74535d4e7e28614a9ffe1c37b98c310782f80
The-Ayush001 <ayush007arm@.com>
2026-05-20 12:59:25 +0530

    TC:1

---
+++ LeetCode/src/Linked_List/Q1_Remove_Nth_node_fromEnd_19.java
@@ -0,0 +1,68 @@
+package Linked_List;
+class Q1_Remove_Nth_node_fromEnd_19 {
+    // creating the node;
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) {
+            this.val = val;
+        }
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    static void main() {
+        // Create l1 = [2,4,3]
+        ListNode l1 = new ListNode(2);
+        l1.next = new ListNode(4);
+        l1.next.next = new ListNode(3);
+
+        // Create l2 = [5,6,4]
+        ListNode l2 = new ListNode(5);
+        l2.next = new ListNode(6);
+        l2.next.next = new ListNode(4);
+
+        // Call function
+        ListNode result = addTwoNumbers(l1, l2);
+
+        // Print result
+        while (result != null) {
+            System.out.print(result.val + " ");
+            result = result.next;
+        }
+
+    }
+
+    // Main solution:
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        ListNode dummy = new ListNode(0);
+        ListNode temp = dummy;
+
+        int carry = 0;
+
+        while (l1 != null || l2 != null || carry != 0) {
+
+            int sum = carry;
+
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+
+            carry = sum / 10;
+
+            temp.next = new ListNode(sum % 10);
+            temp = temp.next;
+        }
+        return dummy.next;
+    }
+}
