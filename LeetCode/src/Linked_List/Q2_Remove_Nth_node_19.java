d0bfb8940ac34c1853ff38ece91ea4d46eb9469a
The-Ayush001 <ayush007arm@.com>
2026-05-21 23:40:05 +0530

    Remove Nth node, Beats 100%

---
+++ LeetCode/src/Linked_List/Q2_Remove_Nth_node_19.java
@@ -0,0 +1,45 @@
+package Linked_List;
+
+public class Q2_Remove_Nth_node_19 {
+
+      public class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+
+  // Main solution:
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+
+        if(head.next == null){
+            return null;
+        }
+        ListNode temp = head;
+        ListNode ptr = head;
+
+        //Move ptr forward n time
+        int i = 1;
+        while(i <= n){
+            ptr = ptr.next;
+            i++;
+        }
+
+        if(ptr == null){  // ptr = null then return the head.next node
+            return head.next;
+        }
+
+        // move both until ptr reach the last node
+        while(ptr.next != null){
+            ptr = ptr.next;
+            temp = temp.next;
+        }
+
+        // skip the nnt node form end.
+        if(temp.next != null){
+            temp.next = temp.next.next;
+        }
+        return head;
+    }
+}
