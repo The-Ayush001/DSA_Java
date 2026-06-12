b002736a2dbfda24fcf7807d69b667d2ef951f93
The-Ayush001 <ayush007arm@gmail.com>
2026-06-12 23:32:17 +0530

    BT Bottom-up traversal

---
+++ LeetCode/src/Tree/Q5_Level_Order_Traversal_II_107.java
@@ -0,0 +1,41 @@
+package Tree;
+
+import java.util.*;
+
+//  return the bottom-up level order traversal of its nodes' values.
+
+public class Q5_Level_Order_Traversal_II_107 {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        // Base condition
+        if(root == null){
+            return result;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            List<Integer> list= new ArrayList<>(size); // list for store the leve node value.
+
+            for(int i=0; i < size; i++){
+                TreeNode currentNode = queue.poll(); // remove one node for front side.
+                list.add(currentNode.val); // add the leve node value in list
+
+                if(currentNode.left != null){ // if left child is exist.
+                    queue.offer(currentNode.left);
+                }
+                if(currentNode.right != null){  // if right child is exist.
+                    queue.offer(currentNode.right);
+                }
+            }
+
+            // add the list in final list at index 0. -> [[3]] -> [[9,20],[3]] -> [[15,7],[9,20],[3]]
+            result.add(0, list);
+        }
+
+        return result;
+    }
+}
